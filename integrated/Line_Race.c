8baf3a61860398a905d3e23a5292990e1b1846ae
JJ Oswald <jjo50@pitt.edu>
2024-02-29 13:56:45 -0500

    cleaned up formatting

--- integrated/Line_Race.c
+++ integrated/Line_Race.c
@@ -10,54 +10,27 @@
 #include "../inc/SysTickInts.h"
 #include "../inc/bump.h"
 
+/*
+ *  dark     --- 0x00
+ *  red      R-- 0x01
+ *  green    -G- 0x02
+ *  yellow   RG- 0x03
+ *  blue     --B 0x04
+ *  pink     R-B 0x05
+ *  sky blue -GB 0x06
+ *  white    RBG 0x07
+*/
 
-void Led_Output(uint8_t data){  // write output to P1.0
-  P1->OUT = data;
-}
+// ---------------------------------------------------
+// FSM Initialization
 
-void Port2_Output(uint8_t data){  // write three outputs bits of P2
-  P2->OUT = (P2->OUT&0xF8)|data;
-}
-
-uint8_t LineResult,BumpResult;
-int FSM_Input(void){
-    //Reflectance
-    if (LineResult==0x00){ return 0; }//0x00
-    else if (LineResult< 0x08){ return 1; }//xxxx,1___ ; too right go left
-    else if (LineResult> 0x10){ return 2; }//___1,xxxx ; too left go right
-    else                      { return 3; }//xxx1,1xxx ; center
-}
-
-
-int count=0,delay_systick=1;
-
-void SysTick_Handler(void){
-  if (count%10==0){
-      Reflectance_Start();
-      count +=1;
-  }else if (count%(10+delay_systick)==0){
-      LineResult = Reflectance_End();
-      //BumpResult = Bump_Read();
-      //Led_Refl();//keep one commented out
-      //Led_Bump();
-      count=0;
-  }else {
-      count +=1;
-  }
-}
-
-
-
-
-// Linked data structure
 typedef struct State {
-  uint8_t  out;                // LED output to mark state (debug)
-  void (*motorFunction)(uint16_t, uint16_t);
-  uint16_t motorSpeed_L;       // speed of Left motor
-  uint16_t motorSpeed_R;       // speed of Right motor
-  uint32_t time_length;        // time motors are enganged
-  const struct State *next[4]; // next if 2-bit input is 0-3
-
+    uint8_t  out;                              // LED output to mark state (debug)
+    void (*motorFunction)(uint16_t, uint16_t); // reference to desired motor function call
+    uint16_t motorSpeed_L;                     // speed of Left motor
+    uint16_t motorSpeed_R;                     // speed of Right motor
+    uint32_t time_length;                      // time motors are engaged
+    const struct State *next[4];               // next if 2-bit input is 0-3
 } State_t;
 
 #define center      &fsm[0] // S1
@@ -69,17 +42,6 @@ typedef struct State {
 #define LostR       &fsm[6] // S7
 #define LostGo      &fsm[7] // S8
 #define LostStop    &fsm[8] // S9
-
-/*
- *  dark     --- 0x00
- *  red      R-- 0x01
- *  green    -G- 0x02
- *  yellow   RG- 0x03
- *  blue     --B 0x04
- *  pink     R-B 0x05
- *  sky blue -GB 0x06
- *  white    RBG 0x07
-*/
 
 State_t fsm[9]={
     {0x01,   &Motor_Forward, 5000, 5000,  500, { center, L1, R1, center }},                   // S1 C         red
@@ -93,8 +55,62 @@ State_t fsm[9]={
     {0x00, &Motor_Stop_Pars,    0,    0,  500, { LostStop, LostStop, LostStop, LostStop }}    // S9 Lost Stop red
 };
 
+// ---------------------------------------------------
+// Other Global Variables
 
-int main(void){
+
+// ---------------------------------------------------
+// Functions
+
+// write output to P1.0
+void Led_Output(uint8_t data) {
+    P1->OUT = data;
+}
+
+// write three outputs bits of P2
+void Port2_Output(uint8_t data) {
+    P2->OUT = (P2->OUT&0xF8)|data;
+}
+
+uint8_t LineResult,BumpResult;
+int FSM_Input(void){
+    //Reflectance
+    if (LineResult==0x00) {
+        return 0; //0x00
+    }
+    else if (LineResult< 0x08){
+        return 1; //xxxx,1___ ; too right go left
+    }
+    else if (LineResult> 0x10) {
+        return 2; //___1,xxxx ; too left go right
+    }
+    else {
+        return 3; //xxx1,1xxx ; center
+    }
+}
+
+
+int count=0,delay_systick=1;
+
+void SysTick_Handler(void) {
+    if (count % 10 == 0) {
+        Reflectance_Start();
+        count +=1;
+    }
+    else if (count % (10 + delay_systick) == 0) {
+        LineResult = Reflectance_End();
+        //BumpResult = Bump_Read();
+        //Led_Refl();//keep one commented out
+        //Led_Bump();
+        count = 0;
+    }
+    else {
+            count += 1;
+    }
+}
+
+// ---------------------------------------------------
+int main(void) {
     Clock_Init48MHz();
     SysTick_Init(48000,2);//64Hz
     LaunchPad_Init();
@@ -102,7 +118,6 @@ int main(void){
     //Bump_Init();
 
     //adjust motor speeds in testing
-
 
     State_t *Spt;  // pointer to the current state
     //int Input;
@@ -114,10 +129,7 @@ int main(void){
     while(1){
         //Output = Spt->out;
         Port2_Output(Spt->out);
-
         Clock_Delay1ms(3000);
-
-
 
         //Input = FSM_Input();
         Spt = Spt->next[FSM_Input()];
