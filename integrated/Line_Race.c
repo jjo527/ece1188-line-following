709e8b3dedd00afb7b53b52bbfaf187e438618de
Adam Brower <adamjbrower17@gmail.com>
2024-03-06 16:44:57 -0500

    fixed fsm turn states

--- integrated/Line_Race.c
+++ integrated/Line_Race.c
@@ -79,11 +79,11 @@ enum FsmInput {
 //  HL L3 L2 L1 | GO | R1 R2 R3 HR
 //  << <^ <^ <^ | ^^ | ^> ^> ^> >>
 
-#define BASE_SPEED 2500
+#define BASE_SPEED 2900
 #define TURN_3 1.1 * BASE_SPEED
 #define TURN_2 1.05 * BASE_SPEED
 #define TURN_1 1.05 * BASE_SPEED
-#define TURN_SPEED 2750
+#define TURN_SPEED 3400
 #define MULT 2
 
 State_t fsm[10]= {
@@ -94,9 +94,9 @@ State_t fsm[10]= {
 
     {0x05,   &Motor_Forward  , BASE_SPEED, BASE_SPEED , { FSM_L4, FSM_L3, FSM_L2, FSM_L1, FSM_C, FSM_R1, FSM_R2, FSM_R3, FSM_R4, FSM_L4}},
 
-    {0x04,   &Motor_Forward  , BASE_SPEED, TURN_1     , { FSM_L4, FSM_L3, FSM_L2, FSM_L1, FSM_C, FSM_R1, FSM_R2, FSM_R3, FSM_R4, FSM_R4}},
-    {0x03,   &Motor_Forward  , BASE_SPEED, TURN_2     , { FSM_L4, FSM_L3, FSM_L2, FSM_L1, FSM_C, FSM_R1, FSM_R2, FSM_R3, FSM_R4, FSM_R4}},
-    {0x02,   &Motor_Forward  , TURN_3    , TURN_3     , { FSM_L4, FSM_L3, FSM_L2, FSM_L1, FSM_C, FSM_R1, FSM_R2, FSM_R3, FSM_R4, FSM_R4}},
+    {0x04,   &Motor_Forward  , TURN_1, BASE_SPEED     , { FSM_L4, FSM_L3, FSM_L2, FSM_L1, FSM_C, FSM_R1, FSM_R2, FSM_R3, FSM_R4, FSM_R4}},
+    {0x03,   &Motor_Forward  , TURN_2, BASE_SPEED     , { FSM_L4, FSM_L3, FSM_L2, FSM_L1, FSM_C, FSM_R1, FSM_R2, FSM_R3, FSM_R4, FSM_R4}},
+    {0x02,   &Motor_Forward  , TURN_3, BASE_SPEED     , { FSM_L4, FSM_L3, FSM_L2, FSM_L1, FSM_C, FSM_R1, FSM_R2, FSM_R3, FSM_R4, FSM_R4}},
     {0x01,   &Motor_Right    , TURN_SPEED, TURN_SPEED , { FSM_L4, FSM_L3, FSM_L2, FSM_L1, FSM_C, FSM_R1, FSM_R2, FSM_R3, FSM_R4, FSM_R4}},
 
     {0x01,   &Motor_Backward , BASE_SPEED, BASE_SPEED , { FSM_L4, FSM_L3, FSM_L2, FSM_L1, FSM_C, FSM_R1, FSM_R2, FSM_R3, FSM_R4, FSM_LOST}},
@@ -116,19 +116,14 @@ uint8_t g_BumpResult;
 // ---------------------------------------------------
 // Functions
 
-// write output to P1.0
-void Led_Output(uint8_t data) {
-    P1->OUT = data;
-}
-
 // write three outputs bits of P2
 void Port2_Output(uint8_t data) {
     P2->OUT = (P2->OUT&0xF8) | data;
 }
 
 int FSM_Input(void){
-    // if (g_LineResult&0x10 && g_LineResult&0x08) { // center XXX1 1XXX
-        // return I_C;
+//    if (g_LineResult&0x10 && g_LineResult&0x08) { // center XXX1 1XXX
+//        return I_C;
     if(g_LineResult&0x10) { // L1 XXX1 XXXX
         return I_L1;
     } else if(g_LineResult&0x08) { // R1 XXXX 1XXX
